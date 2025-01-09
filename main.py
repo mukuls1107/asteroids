e768ec7cf8e50cdcb8d8ab4ad0e17a7cd9a61376
mukuls1107 <mukulsingh3078@gmail.com>
2025-01-09 22:33:07 +0530

    setup

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
-
+from constants import *
+ 
 def main():
     print("Starting asteroids!")
-
+    print("SCREEN WIDTH", SCREEN_WIDTH)
+    print("SCREEN HEiGHT", SCREEN_HEIGHT)
 
 if __name__ == "__main__":
     main()
