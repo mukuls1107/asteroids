22f75b605616a0bd1b0dda19b2132f616acf43b4
mukuls1107 <mukulsingh3078@gmail.com>
2025-01-10 22:39:26 +0530

    player can be control now!

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from constants import *
  
 pygame.init()
 
+
 ck = pygame.time.Clock()
 dt = 0
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+pygame.display.set_caption("Abe Niche Dekh!")
     
+
+updatable = pygame.sprite.Group()
+drawable  = pygame.sprite.Group()
+
+
 def main():
     print("Starting asteroids!")
     print("Screen width:", SCREEN_WIDTH)
@@ -18,12 +24,20 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        drawable.add(pl)
+        updatable.add(pl)
         screen.fill((0,0,0))
-        pl.draw(screen)
+        # pl.draw(screen)
+        for dr in drawable:
+            dr.draw(screen)
+        
         pygame.display.flip()
         delt = ck.tick(60)
         dt = delt / 1000
-        pl.update(dt)
+        # pl.update(dt)
+        for up in updatable:
+            up.update(dt)
         
 if __name__ == "__main__":
     main()
