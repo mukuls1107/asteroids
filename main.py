2c5b7d590c345cdab45b944f90602b10c216c4d4
mukuls1107 <mukulsingh3078@gmail.com>
2025-01-10 22:09:33 +0530

    rotate left and right

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import pygame
+import pygame, player
 from constants import *
  
 pygame.init()
+
+ck = pygame.time.Clock()
+dt = 0
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     
@@ -10,13 +13,17 @@ def main():
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
 
+    pl = player.Player(x=SCREEN_WIDTH/2,y=SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        pl.draw(screen)
         pygame.display.flip()
-        
+        delt = ck.tick(60)
+        dt = delt / 1000
+        pl.update(dt)
         
 if __name__ == "__main__":
     main()
